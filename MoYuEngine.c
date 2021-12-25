d6a7a25fb437a65e1fd9af58dcdffdaaf1067f54
WilsonVinson <cprewilsonvinson@gmail.com>
2021-12-25 19:07:29 +0800

    main

--- MoYuEngine.c
+++ MoYuEngine.c
@@ -1,11 +1,11 @@
 ﻿// MoYuEngine.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("Hello World!\n");
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
