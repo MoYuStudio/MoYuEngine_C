ed9b39df9878d91c15e745dcabc7a7ee38584173
WilsonVinson <cprewilsonvinson@gmail.com>
2021-12-29 16:15:27 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,47 +1,91 @@
 ﻿
 #include <stdio.h>
 #include <SDL.h>
+//#include <SDL2/SDL_image.h>
 
 int width = 400;
 int height = 300;
 
+int framrate = 60;
+
+int fps_o = 5;
+int fps = 0;
 int x = 0;
 
-void blit(SDL_Surface* screen, SDL_Window* win) {
+SDL_Window* window = NULL;
+SDL_Surface* surface = NULL;
 
-    x++;
+void init(){
 
-    if (x == 300){
-        x = 0;}
+    if (SDL_Init(SDL_INIT_VIDEO) < 0){
+        exit(-1);}
 
-    SDL_Rect r = { 0, 0, width, height };
-    SDL_FillRect(screen, &r, 0xffffffff);// ARGB
+    return 0;}
+
+void blit(SDL_Surface* surface, SDL_Window* window){
+
+    if (x <= 0) {
+        fps = fps_o;
+    }
+        
+    if (x >= 300){
+        fps = -fps_o;
+    }
+
+    x += fps;
+
+    SDL_Rect background = { 0, 0, width, height };
+    SDL_FillRect(surface, &background, 0xffffffff);// ARGB
+
     struct SDL_Rect redRect = { x, 0, 100, 100 };
-    SDL_FillRect(screen, &redRect, 0xffff0000);
-    SDL_UpdateWindowSurface(win);}
+    SDL_FillRect(surface, &redRect, 0xffff0000);
 
-void event(SDL_Surface* screen, SDL_Window* win){
+    SDL_UpdateWindowSurface(window);}
 
-    while (1){
-        blit(screen, win);
+void event(SDL_Surface* surface, SDL_Window* window) {
+
+    while (1) {
+
+        uint32_t begin = SDL_GetTicks();
+
+        blit(surface, window);
 
         SDL_Event event;
-        if (SDL_PollEvent(&event)){
-            if (event.type == SDL_QUIT){
-                break;}}}}
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+
+                //SDL_Quit();
+
+                return;
+            }
+        }
+
+        long current = SDL_GetTicks();
+        long cost = current - begin;
+        long frame = 1000 / framrate;
+        long delay = frame - cost;
+
+        if (delay > 0) {
+            SDL_Delay(delay);
+        }
+    }
+}
 
 int main(){
 
-    SDL_Window* win = SDL_CreateWindow("MoYu Engine C",
-                                        SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
-                                        width, height,
-                                        SDL_WINDOW_SHOWN);
+    init();
 
-    SDL_Surface* screen = SDL_GetWindowSurface(win);
+    window = SDL_CreateWindow("MoYu Engine C",
+                                SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
+                                width, height,
+                                SDL_WINDOW_SHOWN);
 
-    SDL_UpdateWindowSurface(win);\
+    surface = SDL_GetWindowSurface(window);
 
-    event(screen, win);
-    SDL_DestroyWindow(win);
+    SDL_UpdateWindowSurface(window); \
+
+    event(surface, window);
+
+    SDL_DestroyWindow(window);
 
     return 0;}
