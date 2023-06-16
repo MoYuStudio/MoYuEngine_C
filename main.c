874697a03c70e67e296d3a4274bd18d4c575d35d
WilsonVinson <cprewilsonvinson@gmail.com>
2023-06-16 11:59:11 +0800

    main

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 
 #include "raylib.h"
 
+float GenerateNoise(int x, int y, int seed)
+{
+    int n = x + y * 57 + seed * 131;
+    n = (n << 13) ^ n;
+    return (1.0f - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7FFFFFFF) / 1073741824.0f);
+}
+
+void GenerateMap(Texture2D texture, int mapSize, float tileSize, int seed)
+{
+    for (int y = 0; y < mapSize; y++)
+    {
+        for (int x = 0; x < mapSize; x++)
+        {
+            float noise = GenerateNoise(x, y, seed);
+            float tileX = (x - y) * tileSize / 2.0f + noise * tileSize / 2.0f;
+            float tileY = (x + y) * tileSize / 4.0f + noise * tileSize / 4.0f;
+            Vector2 position = { tileX, tileY };
+            DrawTextureEx(texture, position, 0.0f, 1.0f, WHITE);
+        }
+    }
+}
 int main(void)
 {
     const int screenWidth = 1280;
@@ -26,14 +47,14 @@ int main(void)
     camera.rotation = 0.0f;
     camera.zoom = 7.0f;
 
-    const int mapSize = 9; // 地图尺寸
-    const float tileSize = 16.0f; // 格子大小
+    const int mapSize = 9;
+    const float tileSize = 16.0f;
+    const int seed = 0; // 设置一个种子值
 
     //SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        // 处理相机移动
         if (IsKeyDown(KEY_W))
             camera.target.y += tileSize * camera.zoom * GetFrameTime();
         if (IsKeyDown(KEY_S))
@@ -43,25 +64,13 @@ int main(void)
         if (IsKeyDown(KEY_D))
             camera.target.x -= tileSize * camera.zoom * GetFrameTime();
 
-
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
             BeginMode2D(camera);
 
-                // 绘制地图
-                for (int y = 0; y < mapSize; y++)
-                {
-                    for (int x = 0; x < mapSize; x++)
-                    {
-                        // 计算每个格子的位置
-                        float tileX = (x - y) * tileSize / 2.0f;
-                        float tileY = (x + y) * tileSize / 4.0f;
-                        Vector2 position = { tileX, tileY };
-                        DrawTextureEx(texture, position, 0.0f, 1.0f, WHITE);
-                    }
-                }
+                GenerateMap(texture, mapSize, tileSize, seed);
 
             EndMode2D();
 
