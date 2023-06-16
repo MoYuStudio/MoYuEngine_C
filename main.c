f6cfd5f77596e5da15c5a0b9de5d2c2d54617fd4
WilsonVinson <cprewilsonvinson@gmail.com>
2023-06-16 11:50:29 +0800

    main

--- main.c
+++ main.c
@@ -26,18 +26,23 @@ int main(void)
     camera.rotation = 0.0f;
     camera.zoom = 7.0f;
 
+    const int mapSize = 9; // 地图尺寸
+    const float tileSize = 16.0f; // 格子大小
+
     //SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        // 处理相机移动
         if (IsKeyDown(KEY_W))
-            camera.target.y += speed * GetFrameTime();
+            camera.target.y += tileSize * camera.zoom * GetFrameTime();
         if (IsKeyDown(KEY_S))
-            camera.target.y -= speed * GetFrameTime();
+            camera.target.y -= tileSize * camera.zoom * GetFrameTime();
         if (IsKeyDown(KEY_A))
-            camera.target.x += speed * GetFrameTime();
+            camera.target.x += tileSize * camera.zoom * GetFrameTime();
         if (IsKeyDown(KEY_D))
-            camera.target.x -= speed * GetFrameTime();
+            camera.target.x -= tileSize * camera.zoom * GetFrameTime();
+
 
         BeginDrawing();
 
@@ -45,7 +50,18 @@ int main(void)
 
             BeginMode2D(camera);
 
-                DrawTextureEx(texture, position, 0.0f, 1.0f, WHITE);
+                // 绘制地图
+                for (int y = 0; y < mapSize; y++)
+                {
+                    for (int x = 0; x < mapSize; x++)
+                    {
+                        // 计算每个格子的位置
+                        float tileX = (x - y) * tileSize / 2.0f;
+                        float tileY = (x + y) * tileSize / 4.0f;
+                        Vector2 position = { tileX, tileY };
+                        DrawTextureEx(texture, position, 0.0f, 1.0f, WHITE);
+                    }
+                }
 
             EndMode2D();
 
